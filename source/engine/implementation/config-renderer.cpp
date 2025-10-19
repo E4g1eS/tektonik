779055cf4039c80edc83d999083590a5eeef8fdd
Richard Smely <smelyrich@gmail.com>
2025-10-19 11:25:33 +0200

    WIP

--- source/engine/implementation/config-renderer.cpp
+++ source/engine/implementation/config-renderer.cpp
@@ -1,14 +1,21 @@
 module;
 #include "imgui-wrapper.hpp"
+#include "sdl-wrapper.hpp"
 #include "std.hpp"
 module config_renderer;
+
+import vulkan;
 
 namespace tektonik::config
 {
 
 void Renderer::Init()
 {
-    // TODO
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+
+
 }
 
 }  // namespace tektonik::config
